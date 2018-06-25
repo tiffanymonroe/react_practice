5d4dae151e0898bd5f5b19126c8c448fd7524993
Tiffany Monroe <hello@tiffanymonroe.com>
2018-06-24 22:48:08 -0700

    lotr exercise

--- lord-of-the-rings/src/App.js
+++ lord-of-the-rings/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Movie title="The Fellowhip of the Ring" hours="2" minutes="58"></Movie>
+        <Movie title="The Two Towers" hours="2" minutes="59"></Movie>
+        <Movie title="The Return of the King" hours="3" minutes="21"></Movie>
       </div>
     );
   }
--- lord-of-the-rings/src/Movie.js
+++ lord-of-the-rings/src/Movie.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class Movie extends Component {
   render () {
     return (
-
+      <div>
+        <h1>The Lord of the Rings: {this.props.title}</h1>
+        <p>{this.props.hours}h {this.props.minutes}min</p>
+      </div>
     )
   }
 }
