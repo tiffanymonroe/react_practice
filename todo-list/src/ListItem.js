9fae8b66164cc45d55699b1e78a17dc5fa2d6f36
Tiffany Monroe <hello@tiffanymonroe.com>
2018-06-27 22:53:05 -0700

    changed ListItem.js to a functional component

--- todo-list/src/ListItem.js
+++ todo-list/src/ListItem.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import './App.css'
 
-class ListItem extends Component {
-
-  render(){
-    return(
-      <li>{this.props.doThis}</li>
-    )
-  }
-}
+const ListItem = props => (
+  <div>
+    <li>{props.doThis}</li>
+  </div>
+)
 
 export default ListItem
