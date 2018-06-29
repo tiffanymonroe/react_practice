300d869c8b7c666bfbb8565b8fe054812cc246e0
Tiffany Monroe <hello@tiffanymonroe.com>
2018-06-28 20:26:14 -0700

    fixed app.js

--- fruit-list/src/App.js
+++ fruit-list/src/App.js
@@ -58,42 +58,13 @@ const fruitList = [
 //Fruit Container
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      //initialize the fruit list to the full list passed in props
-
-      fruitsToDisplay: props.fruits,
-
-      //initialize the filter to an empty string
-      filterValue: ''
-    }
-  }
-
-  handleFilterChange(e){
-    e.preventDefault()
-    const filterValue = e.target.value
-    this.setState((prevState, props)=>{
-      //remove fruits that don't contain the filter value
-      const filteredFruitList = props.fruits.filter(fruit => fruit.toLocaleLowerCase().includes(filterValue.ttoLocaleLowerCase()))
-      //return new state with filtered fruit list and the new value of the filter
-      return {
-        fruitsToDisplay: filteredFruitList,
-        filterValue
-      }
-    })
-  }
-
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <main>
+
         <FruitContainer fruits={fruitList} />
-        </main>
+
       </div>
     );
   }
