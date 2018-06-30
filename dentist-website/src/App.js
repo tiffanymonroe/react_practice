da2f25e7b0b8c4957f60575585f2a3eb145f7e2b
Tiffany Monroe <hello@tiffanymonroe.com>
2018-06-30 10:53:27 -0700

    dentist website setup

---
+++ dentist-website/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Website</h1>
+        <p>Lorem ipsum Swag chia art party organic. Freegan umami hell of, post-ironic semiotics poke migas snackwave put a bird on it. Beard ramps hashtag twee fashion axe selfies readymade kickstarter. Paleo offal squid butcher. Tofu waistcoat photo booth plaid prism succulents, chartreuse aesthetic pour-over semiotics banh mi mlkshk. Before they sold out squid quinoa, church-key copper mug direct trade authentic retro bicycle rights cloud bread irony af distillery put a bird on it readymade.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
