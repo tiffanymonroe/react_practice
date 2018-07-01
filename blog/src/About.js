a5ca07f35775cab82a219c9cdabe943b871d9cca
Tiffany Monroe <hello@tiffanymonroe.com>
2018-07-01 00:51:53 -0700

    weather api added

--- blog/src/About.js
+++ blog/src/About.js
@@ -1,11 +1,57 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class About extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      zipcode: '',
+      highTemp: 0,
+      lowTemp: 0,
+      city: '',
+      description: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e){
+    this.setState({zipcode: e.target.value})
+    console.log("Your zipcode is: " + this.state.zipcode);
+  }
+  handleSubmit(e){
+    let base = this;
+
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + base.state.zipcode + ',us&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial')
+      .then((res)=>{
+        return res.json()
+      }).then((json)=>{
+        console.log(json);
+        base.setState({
+          city: json.name,
+          description: json.weather[0].description,
+          highTemp: json.main.temp_max,
+          lowTemp: json.main.temp_min
+        })
+      }).catch((err)=>{
+        console.log('oops! the error is: ', err);
+      })
+      e.preventDefault()
+  }
   render(){
     return(
       <div>
         <p>Intelligentsia church-key man bun poke. Pitchfork irony affogato portland. Lumbersexual meditation knausgaard microdosing offal. Lumbersexual ennui raclette, tousled direct trade food truck master cleanse waistcoat church-key austin pour-over twee iPhone meggings yuccie.
         </p>
+        <form onSubmit={this.handleSubmit}>
+          <p>{this.state.city}: currently {this.state.description} with a high of {this.state.highTemp} degress and a low of {this.state.lowTemp} degrees.</p>
+          <label>
+            Please enter your zipcode for the weather:
+            <br/>
+            <input type="text" onChange={this.handleChange} />
+          </label>
+          <br/>
+          <input type="submit" value="Get my forecast!" />
+        </form>
       </div>
     )
   }
--- blog/src/App.js
+++ blog/src/App.js
@@ -21,33 +21,36 @@ const post = {
   comments: ["First!", "Great post", "Hire this author now!"]
 }
 
-const App = () => (
+class App extends Component {
+ render(){
+   return(
+     <Router>
+     <div>
+       <nav>
+         <Link to="/">Home</Link>
+         <Link to="/about">About moi</Link>
+         <Link to="/blog">My blog</Link>
+         <Link to="/food">Favorite noms</Link>
+         <Link to="/movie">Favorite Movie</Link>
+       </nav>
+       <hr/>
 
-      <Router>
-      <div>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About moi</Link>
-          <Link to="/blog">My blog</Link>
-          <Link to="/food">Favorite noms</Link>
-          <Link to="/movie">Favorite Movie</Link>
-        </nav>
-        <hr/>
+       <Route exact path="/" component={Home}/>
+       <Route path="/about" component={About}/>
+       <Route path="/blog" component={
+             () => (<Blog title={post.title}
+             allAuthors={post.authors}
+             body={post.body}
+             comments={post.comments} />
+       )}/>
+       <Route path="/food" component={Food}/>
+             <Route path="/movie" component={Movie}/>
+     </div>
+     </Router>
 
-        <Route exact path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/blog" component={
-              () => (<Blog title={post.title}
-              allAuthors={post.authors}
-              body={post.body}
-              comments={post.comments} />
-        )}/>
-        <Route path="/food" component={Food}/>
-              <Route path="/movie" component={Movie}/>
-      </div>
-      </Router>
-
-)
+   )
+ }
+}
 
 
 
